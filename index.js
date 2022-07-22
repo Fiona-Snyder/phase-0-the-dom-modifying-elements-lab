61b899a383cb42767d73f228c26815a02ab75008
Fiona Snyder <fiona.snyder@outlook.com>
2022-07-22 15:54:16 -0500

    commit tree 6f9b1ee406f3b4faaf0ac696486b60285c6bd6f9

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code here!
+const m = document.getElementById("main")
+m.remove()
+
+const newHeader = document.createElement("h1")
+
+newHeader.id = "victory"
+
+/* Either textContent or innerHTML will work in this example. 
+newHeader.innerHTML = "Fiona is the champion"*/
+newHeader.textContent = "Fiona is the champion"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
